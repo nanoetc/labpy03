537cf5f00f34bc50b2df80c85df8be081a7f4e73
bagus <xtkj1.07.bagus@gmail.com>
2020-10-30 14:52:28 +0700

    lab 2 pertemuan ke 6

---
+++ lab 2/penggabungan variable.py
@@ -0,0 +1,8 @@
+#konversi nilai variable
+a=int(input("masukkan nilai a="))
+b=int(input("masukkan nilai b="))
+print("variable a=",a)
+print("variable b=",b)
+print("hasil penggabungan {0}&{1}=%s".format(a,b) %(a&b))
+print("hasil penjumlahan {0}+{1}=%s".format(a,b) %(a+b))
+print("hasil penggabungan {0}/{1}=%s".format(a,b) %(a/b))
