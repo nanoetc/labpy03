76041cbe0809a49e0639fa6740be59265cd81b12
bagus <xtkj1.07.bagus@gmail.com>
2020-10-30 14:41:31 +0700

    lab 2 pertemuan ke 6

---
+++ lab 2/lab1 penggunaan end.py
@@ -0,0 +1,8 @@
+# penggunaan end
+print('A', end='')
+print('B', end='')
+print('C', end='')
+print()
+print('A')
+print('Y')
+print('Z')
---
+++ lab 2/lab1 penggunaan separator.py
@@ -0,0 +1,7 @@
+#penggunaan separator
+w,x,y,z = 10,15,20,25
+print(w,x,y,z)
+print(w,x,y,z,sep=',')
+print(w,x,y,z,sep='')
+print(w,x,y,z,sep=':')
+print(w,x,y,z,sep='-----')
